d826839c839455578f67a9a8bdfa29182245f214
Gary Chambers <gary.chambers@privitar.com>
2018-06-23 10:07:39 +0100

    Add root project files to search UI service

---
+++ services/search/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const mode = "development";
+
+module.exports = {
+    mode,
+
+    entry: "./src/client/index.tsx",
+
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/",
+    },
+
+    resolve: {
+        alias: {
+            "~modules": path.resolve(__dirname, "src/client/modules"),
+        },
+
+        extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+
+    module: {
+        rules: [
+            { test: /\.tsx?$/, exclude: /node_modules/, use: "ts-loader" },
+            { test: /\.css$/, exclude: /node_modules/, use: ["style-loader", "css-loader"] },
+        ],
+    },
+};
